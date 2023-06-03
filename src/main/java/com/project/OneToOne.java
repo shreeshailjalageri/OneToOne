90c029aba1d11fb94ccd9f74fdc0940890018ac6
shreeshailjalageri <smjalageri400@gmail.com>
2023-06-03 12:10:18 +0530

    updated

--- src/main/java/com/project/OneToOne.java
+++ src/main/java/com/project/OneToOne.java
@@ -1,12 +1,15 @@
 package com.project;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class OneToOne {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
 	private String address;
@@ -14,6 +17,54 @@ public class OneToOne {
 	
 	@javax.persistence.OneToOne
 	private Pancard pancard;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public Pancard getPancard() {
+		return pancard;
+	}
+
+	public void setPancard(Pancard pancard) {
+		this.pancard = pancard;
+	}
+
+	@Override
+	public String toString() {
+		return "OneToOne [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + ", pancard="
+				+ pancard + "]";
+	}
+	
+	
 	
 }
  
--- src/main/java/com/project/Pancard.java
+++ src/main/java/com/project/Pancard.java
@@ -1,5 +1,47 @@
 package com.project;
 
-public class Pancard {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Pancard {
+ 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	private String name;
+	private String address;
+	private long phone;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	@Override
+	public String toString() {
+		return "Pancard [id=" + id + ", name=" + name + ", address=" + address + ", phone=" + phone + "]";
+	}
+	
+	
 }
